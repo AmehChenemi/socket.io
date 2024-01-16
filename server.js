dd0f3b20b7e66c0fb07277a4c40349fa3ecefdce
AmehChenemi <amehlove147@gmail.com>
2024-01-16 08:06:48 -0200

    recent updates

--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-// const express = require('express')
-// const http = require('http')
-// const socket = require('socket.io')
-
-// const port = 9999
-// const app = express()
-// const server = http.createServer(app)
-// const io = socket(server)
-
-
-// app.set('view engine','ejs')
-
-
-// app.get('/',(req, res)=>{
-//     res.render('homepage')
-// })
-
-// io.on('connection', (socket)=>{
-// socket.on('message',(data)=>{
-//     socket.broadcast.emit('message', data)
-// })
-// })
-// server.listen(port,()=>{
-//     console.log(`server is listening on port ${port}`)
-// })
-
-
 const express = require('express')
 const http = require('http')
 
