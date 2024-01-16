3bd6957c7a39b95b4a4d2d809c0790c2a0b2e567
AmehChenemi <amehlove147@gmail.com>
2024-01-16 08:05:08 -0200

    recent updates

--- server.js
+++ server.js
@@ -1,25 +1,51 @@
+// const express = require('express')
+// const http = require('http')
+// const socket = require('socket.io')
+
+// const port = 9999
+// const app = express()
+// const server = http.createServer(app)
+// const io = socket(server)
+
+
+// app.set('view engine','ejs')
+
+
+// app.get('/',(req, res)=>{
+//     res.render('homepage')
+// })
+
+// io.on('connection', (socket)=>{
+// socket.on('message',(data)=>{
+//     socket.broadcast.emit('message', data)
+// })
+// })
+// server.listen(port,()=>{
+//     console.log(`server is listening on port ${port}`)
+// })
+
+
 const express = require('express')
 const http = require('http')
-const socket = require('socket.io')
 
 const port = 9999
+const socket = require('socket.io')
+
 const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
+app.set("view engine", "ejs")
 
-app.set('view engine','ejs')
-
-
-app.get('/',(req, res)=>{
-    res.render('homepage')
+app.get("/", (req, res)=>{
+ res.render("homepage")
 })
 
-io.on('connection', (socket)=>{
-socket.on('message',(data)=>{
-    socket.broadcast.emit('message', data)
+io.on("connection", (socket)=>{
+   socket.on('chatMessage', (data)=>{
+       socket.broadcast.emit('chatMessage', data)
+   })
 })
-})
-server.listen(port,()=>{
-    console.log(`server is listening on port ${port}`)
+server.listen(port, ()=>{
+    console.log(`server is listening on port:${port}`);
 })
